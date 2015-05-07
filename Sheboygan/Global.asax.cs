03939258a1d7d0d37b6c210a06289cd2ba740720
Jackie Ng <jumpinjackie@gmail.com>
2015-05-07 23:52:05 +1000

    Initial commit

---
+++ Sheboygan/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using OSGeo.MapGuide;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sheboygan
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            MapGuideApi.MgInitializeWebTier(ConfigurationManager.AppSettings["MapGuideWebConfig"]);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
